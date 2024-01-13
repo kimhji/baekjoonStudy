2f8781c006ce81eba2abe8c546232992acd044df
kimhji <126762281+kimhji@users.noreply.github.com>
2024-01-13 22:29:21 +0900

    [Silver III] Title: N과 M (8), Time: 72 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,19 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+numList = list(map(int, sys.stdin.readline().split()))
+
+numList.sort()
+def permutation(num, cmpArray):
+    for inputNum in numList:
+        if len(cmpArray) == 0 or inputNum >= cmpArray[-1]:
+            cmpArray.append(inputNum)
+            if num == M:
+                for printF in range(M):
+                    print(cmpArray[printF], end=' ')
+                print()
+            else:
+                permutation(num + 1, cmpArray)
+            cmpArray.pop()
+
+permutation(1, list())
