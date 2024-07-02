07e5533314277a88ca345e894fa9a24eb388c434
kimhji <126762281+kimhji@users.noreply.github.com>
2024-07-02 21:35:32 +0900

    [Silver IV] Title: 숫자놀이, Time: 108 ms, Memory: 9676 KB -BaekjoonHub

--- 백준/Silver/1755. 숫자놀이/숫자놀이.js
+++ 백준/Silver/1755. 숫자놀이/숫자놀이.js
@@ -1,8 +1,8 @@
 function Number2String(number){
-    const result = [];
+    var result = [];
     number = number.toString();
-    while(number !== ""){
-        const tns = number[0];
+    while(number != ""){
+        tns = number[0];
         switch(tns){
             case "0":
                 result.push("zero");
@@ -42,27 +42,25 @@ function Number2String(number){
     return result.join(" ");
 }
 
-const fs = require("fs");
-const inp = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
-const start = parseInt(inp[0]);
-const end = parseInt(inp[1]);
-const resultList = [];
 
-for(let i = start; i <= end; i++){
-    resultList.push([Number2String(i), i]);
+let fs = require("fs");
+//let inp = fs.readFileSync("javascriptTest/input.txt").toString().split(" ");
+let inp = fs.readFileSync("/dev/stdin").toString().split(" ");
+resultList = [];
+for(var i =parseInt(inp[0]);i<=parseInt(inp[1]);i++){
+    resultList.push([Number2String(i),i]);
 }
-
 resultList.sort((a, b) => a[0].localeCompare(b[0]));
-
-const printList = resultList.map(row => row[1]);
-let line = [];
-for(let i = 0; i < printList.length; i++){
+printList = resultList.map(row=>row[1]);
+var line = [];
+for(var i = 0;i<printList.length;i++){
     line.push(printList[i]);
-    if(i % 10 === 9){
+    if(i%10 == 9){
         console.log(line.join(" "));
         line = [];
     }
 }
 if(line.length > 0){
     console.log(line.join(" "));
+    line = [];
 }
