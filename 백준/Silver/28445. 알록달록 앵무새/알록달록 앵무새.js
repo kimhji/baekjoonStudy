a026da53966d0c9e5cee946b08d4ff51a9e7e308
kimhji <126762281+kimhji@users.noreply.github.com>
2024-07-26 13:26:08 +0900

    [Silver V] Title: 알록달록 앵무새, Time: 136 ms, Memory: 9656 KB -BaekjoonHub

--- 백준/Silver/28445. 알록달록 앵무새/알록달록 앵무새.js
+++ 백준/Silver/28445. 알록달록 앵무새/알록달록 앵무새.js
@@ -1,41 +1,36 @@
-const readline = require('readline');
+const fs = require("fs");
+//let inp = fs.readFileSync("input.txt").toString().split("\n");
+let inp = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+let colorSet = new Set();
+inp.forEach(line =>{
+    let colors = line.trim().split(" ");
+    colors.forEach(color =>{
+        colorSet.add(color);
+    })
+})
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+let colorsArray = Array.from(colorSet);
+let map = [];
 
-let colors = new Set();
-
-// 입력 받기
-rl.on('line', (line) => {
-    line.trim().split(' ').forEach(color => {
-        colors.add(color);
-    });
-});
-
-rl.on('close', () => {
-    let colorsArray = Array.from(colors);
-    let map = [];
-
-    // 모든 색깔 쌍 생성
-    for (let i = 0; i < colorsArray.length; i++) {
-        for (let j = 0; j < colorsArray.length; j++) {
-            map.push([colorsArray[i], colorsArray[j]]);
-        }
+// 모든 색깔 쌍 생성
+for (let i = 0; i < colorsArray.length; i++) {
+    for (let j = 0; j < colorsArray.length; j++) {
+        map.push([colorsArray[i], colorsArray[j]]);
     }
+}
 
-    // 정렬
-    map.sort((a, b) => {
-        if (a[0] !== b[0]) {
-            return a[0].localeCompare(b[0]);
-        } else {
-            return a[1].localeCompare(b[1]);
-        }
-    });
-
-    // 출력
-    map.forEach(pair => {
-        console.log(pair[0] + ' ' + pair[1]);
-    });
+// 정렬
+map.sort((a, b) => {
+    if (a[0] !== b[0]) {
+        return a[0].localeCompare(b[0]);
+    } else {
+        return a[1].localeCompare(b[1]);
+    }
 });
+
+
+let resultList = new Array(...map);
+
+resultList.forEach(item=>{
+    console.log(item[0]+" "+item[1]);
+})
