f64f20b691287a4dbe55f2d8031d81edf4de1b4b
kimhji <126762281+kimhji@users.noreply.github.com>
2024-09-23 12:38:11 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 500 ms, Memory: 95832 KB, Score: 200 point -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+//const N = parseInt(fs.readFileSync("input.txt").toString().trim());
+const N = parseInt(fs.readFileSync("/dev/stdin").toString().trim());
+
+let count = 1;
+const bag = [BigInt(0), BigInt(1)];
+while (count < N) {
+  const curV = (bag[count] + bag[count - 1]) % BigInt(1000000007);
+  bag.push(curV);
+  count += 1;
+}
+
+const result = bag.pop();
+
+console.log(result.toString());
