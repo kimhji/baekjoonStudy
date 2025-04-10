308204cdbcf5df3def52f97e0f4bfcf9728cec54
kimhji <126762281+kimhji@users.noreply.github.com>
2025-04-10 13:20:07 +0900

    [Silver III] Title: 타노스, Time: 144 ms, Memory: 16700 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.java
+++ 백준/Silver/20310. 타노스/타노스.java
@@ -6,14 +6,23 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = br.readLine().trim();
         String result = "";
-        int N = line.length();
-        int num = N;
+        int num = line.length();
         int disappeared_0 = 0;
         int disappeared_1 = 0;
+        for(int cycle = 0;cycle<num;cycle++){
+            if(line.charAt(cycle) == '1'){
+            	disappeared_1++;
+            }
+            else if(line.charAt(cycle) == '0'){
+            	disappeared_0++;
+            }
+        }
+        disappeared_0 /= 2;
+        disappeared_1 /= 2;
         
         for(int cycle = 0;cycle<num;cycle++){
-            if(line.charAt(cycle) == '1' && disappeared_1 < N/4){
-                disappeared_1++;
+            if(line.charAt(cycle) == '1' && disappeared_1 > 0){
+                disappeared_1--;
                 continue;
             }
             result += line.charAt(cycle);
@@ -22,8 +31,8 @@ public class Main{
         result = "";
         num = line.length();
         for(int cycle = num-1;cycle>=0;cycle--){
-            if(line.charAt(cycle) == '0' && disappeared_0 < N/4){
-                disappeared_0++;
+            if(line.charAt(cycle) == '0' && disappeared_0 > 0){
+                disappeared_0--;
                 continue;
             }
             result = line.charAt(cycle) + result;
