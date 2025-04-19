204454b8096a9d01c4d5ecd8d5302febeebc1f57
kimhji <126762281+kimhji@users.noreply.github.com>
2025-04-19 21:11:54 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 392 ms, Memory: 43664 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -1,70 +1,76 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
-public class Main{
-	public static boolean [][] valueBoard;
-	public static String [][] Board;
-	public static int value = 0;
-	public static void main(String[] args) throws IOException{
-		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-		String[] line = sc.readLine().trim().split(" ");
-		int N = Integer.parseInt(line[0]);
-		int M = Integer.parseInt(line[1]);
-		Board = new String[N][M];
-		valueBoard = new boolean[N][M];
-		Integer[] loc = {-1,-1};
-		
-		for(int i = 0;i<N;i++) {
-			Board[i] = sc.readLine().trim().split("");
-			for(int j = 0;j<M;j++) {
-				if(Board[i][j].equals("I")) {
-					loc[0] = i;
-					loc[1] = j;
-				}
-			}
-		}
-		
-		Vector<Integer[]> bag = new Vector<Integer[]>();
-		bag.add(loc);
-		BFS(bag, 1, N, M);
-		System.out.print(value>0?value:"TT");
-	}
-	
-	public static void BFS(Vector<Integer[]> bag, int length, int N, int M) {
-		int nextLength = 0;
-		Integer[] loc;
-		Vector<Integer[]> nextBag = new Vector<Integer[]>();
-		for(int c = 0;c<length;c++) {
-			loc=bag.elementAt(c);
-			if(valueBoard[loc[0]][loc[1]]) continue;
-			if(Board[loc[0]][loc[1]].equals("P")) value++;
-			valueBoard[loc[0]][loc[1]] = true;
-			if(loc[1]>0 && !valueBoard[loc[0]][loc[1]-1] && !Board[loc[0]][loc[1]-1].equals("X")) {
-				Integer[] tnsLoc = {loc[0], loc[1]-1};
-				nextBag.add(tnsLoc);
-				nextLength++;
-			}
-			if(loc[1]+1<M && !valueBoard[loc[0]][loc[1]+1] && !Board[loc[0]][loc[1]+1].equals("X")) {
-				Integer[] tnsLoc = {loc[0], loc[1]+1};
-				nextBag.add(tnsLoc);
-				nextLength++;
-			}
 
-			if(loc[0]>0 && !valueBoard[loc[0]-1][loc[1]] && !Board[loc[0]-1][loc[1]].equals("X")) {
-				Integer[] tnsLoc = {loc[0]-1, loc[1]};
-				nextBag.add(tnsLoc);
-				nextLength++;
-			}
-			if(loc[0]+1<N && !valueBoard[loc[0]+1][loc[1]] && !Board[loc[0]+1][loc[1]].equals("X")) {
-				Integer[] tnsLoc = {loc[0]+1, loc[1]};
-				nextBag.add(tnsLoc);
-				nextLength++;
-			}
-		}
-		if(nextLength == 0) return;
-		BFS(nextBag, nextLength, N, M);
-	}
+public class Main{	
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().trim().split(" ");
+        int N = Integer.parseInt(inputs[0]);
+        int M = Integer.parseInt(inputs[1]);
+        String[] board = new String[N];
+        boolean[][] visitedBoard = new boolean[N][M];
+        boolean initLocFound = false;
+        int result = 0;
+        LinkedList<Integer[]> bag = new LinkedList<Integer[]>();
+        for(int init = 0;init<N;init++) {
+        	board[init] = br.readLine().trim();
+        	if(initLocFound) continue;
+        	for(int init2 = 0;init2<M;init2++) {
+        		if(board[init].charAt(init2) == 'I') {
+        			Integer[] loc = {init, init2};
+        			bag.add(loc);
+        			initLocFound = true;
+        		}
+        	}
+        }
+        int len = 1;
+        while(len>0) {
+        	LinkedList<Integer[]> nextBag = new LinkedList<Integer[]>();
+        	int nextLen = 0;
+        	Integer[] curLoc = bag.pollFirst();
+        	while(curLoc != null) {
+        		if(curLoc[1]<M-1 && board[curLoc[0]].charAt(curLoc[1]+1) != 'X' && !visitedBoard[curLoc[0]][curLoc[1]+1]) {
+        			Integer[] nextLoc = {curLoc[0],curLoc[1]+1};
+        			nextBag.add(nextLoc);
+        			nextLen++;
+        			if(board[curLoc[0]].charAt(curLoc[1]+1) == 'P') result++;
+        			visitedBoard[curLoc[0]][curLoc[1]+1] = true;
+        		}
+        		if(curLoc[0]<N-1 && board[curLoc[0]+1].charAt(curLoc[1]) != 'X' && !visitedBoard[curLoc[0]+1][curLoc[1]]) {
+        			Integer[] nextLoc = {curLoc[0]+1,curLoc[1]};
+        			nextBag.add(nextLoc);
+        			nextLen++;
+        			if(board[curLoc[0]+1].charAt(curLoc[1]) == 'P') result++;
+        			visitedBoard[curLoc[0]+1][curLoc[1]] = true;
+        		}
+        		
+        		if(curLoc[0]>0 && board[curLoc[0]-1].charAt(curLoc[1]) != 'X'&& !visitedBoard[curLoc[0]-1][curLoc[1]]) {
+        			Integer[] nextLoc = {curLoc[0]-1,curLoc[1]};
+        			nextBag.add(nextLoc);
+        			nextLen++;
+        			if(board[curLoc[0]-1].charAt(curLoc[1]) == 'P') result++;
+        			visitedBoard[curLoc[0]-1][curLoc[1]] = true;
+        		}
+        		if(curLoc[1]>0 && board[curLoc[0]].charAt(curLoc[1]-1) != 'X'&& !visitedBoard[curLoc[0]][curLoc[1]-1]) {
+        			Integer[] nextLoc = {curLoc[0],curLoc[1]-1};
+        			nextBag.add(nextLoc);
+        			nextLen++;
+        			if(board[curLoc[0]].charAt(curLoc[1]-1) == 'P') result++;
+        			visitedBoard[curLoc[0]][curLoc[1]-1] = true;
+        		}
+        		
+        		curLoc = bag.pollFirst();
+        	}
+        	
+        	bag = nextBag;
+        	len = nextLen;
+        }
+        if(result == 0) {
+        	System.out.println("TT");
+        }
+        else {
+        	System.out.println(result);
+        }
+    }
 }
