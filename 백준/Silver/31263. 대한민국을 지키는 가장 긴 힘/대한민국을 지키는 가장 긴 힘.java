5932d6306fd6189435e7e35a92bc97b3f6c70ae8
kimhji <126762281+kimhji@users.noreply.github.com>
2025-04-24 23:09:18 +0900

    [Silver III] Title: 대한민국을 지키는 가장 긴 힘, Time: 100 ms, Memory: 14308 KB, Score: 64 point -BaekjoonHub

--- 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.java
+++ 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.java
@@ -10,16 +10,17 @@ public class Main{
         int result = 1;
         line = br.readLine().trim();
         int tnsNumber = 0;
-        for(char one:line.toCharArray()) {
+        char [] ones=line.toCharArray();
+        		
+        for(int i = 0;i<N;i++) {
         	tnsNumber *= 10;
-        	tnsNumber += one - '0';
-        	if(tnsNumber / 100 > 0 && tnsNumber / 10 % 10 != 0 && one == '0') {
-        		result++;
-        		tnsNumber%=100;
-        	}
-        	if(tnsNumber > 641) {
-        		result++;
-        		tnsNumber %= 10;
+        	tnsNumber += ones[i] - '0';
+        	if (tnsNumber > 641) {
+        	    result++;
+        	    tnsNumber = ones[i] - '0';  
+        	} else if (i < N - 1 && ones[i] == '0' && ones[i + 1] == '0' && tnsNumber > 60) {
+        	    result++;
+        	    tnsNumber %= 100;
         	}
         }
         System.out.print(result);
