20232ee38d4e3bedc713cfb4059e6d125d010a99
kimhji <126762281+kimhji@users.noreply.github.com>
2025-08-03 22:22:19 +0900

    [Silver V] Title: 세 부분, Time: 140 ms, Memory: 17116 KB -BaekjoonHub

---
+++ 백준/Silver/2993. 세 부분/세 부분.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        String answer = null;
+        int n = str.length();
+
+        for (int i = 1; i < n - 1; i++) {       
+            for (int j = i + 1; j < n; j++) {    
+                String part1 = new StringBuilder(str.substring(0, i)).reverse().toString();
+                String part2 = new StringBuilder(str.substring(i, j)).reverse().toString();
+                String part3 = new StringBuilder(str.substring(j)).reverse().toString();
+
+                String combined = part1 + part2 + part3;
+
+                if (answer == null || combined.compareTo(answer) < 0) {
+                    answer = combined;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
